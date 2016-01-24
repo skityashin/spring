40e607acc9dd530eb36d8361ee8643dd2645abec
php <skityashin@gmail.com>
2016-01-24 11:30:41 +0200

    1

--- src/main/java/com/levelup/Hello.java
+++ src/main/java/com/levelup/Hello.java
@@ -1,7 +1,10 @@
 package com.levelup;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Hello {
     private String s;
+    private String name;
 
     public Hello(String s) {
         this.s = s;
@@ -11,9 +14,22 @@ public class Hello {
     }
 
     public void sayHello(){
-        System.out.println(s);
+        System.out.println(s + " " + name);
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext(new String[]{"springConfig.xml"});
+        Hello h = (Hello) ac.getBean("hello");
+        h.sayHello();
+        System.out.println();
+    }
 }
