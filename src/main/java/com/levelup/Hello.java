f9e46adb4989f9f2eb75a82b39d6b79afabc7ec0
php <skityashin@gmail.com>
2016-01-24 10:30:54 +0200

    Initial commit

---
+++ src/main/java/com/levelup/Hello.java
@@ -0,0 +1,19 @@
+package com.levelup;
+
+public class Hello {
+    private String s;
+
+    public Hello(String s) {
+        this.s = s;
+    }
+
+    public Hello() {
+    }
+
+    public void sayHello(){
+        System.out.println(s);
+    }
+
+
+
+}
